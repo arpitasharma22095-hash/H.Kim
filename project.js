9fe6b7f6d2774330963fcaeca18b8c35566dea4b
Arpita Sharma <arpitasharma22095@gmail.com>
2026-03-20 16:25:00 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,26 @@
+alert("Welcome to Our Website")
+const btn = document.querySelector(".hut3 button");
+
+btn.addEventListener("click", function() {
+    alert("Booked!");
+});
+const bto = document.querySelector(".hut2 button");
+
+bto.addEventListener("click", function() {
+    alert("Booked!");
+});
+const bro = document.querySelector(".hut4 button");
+
+bro.addEventListener("click", function() {
+    alert("Liked!");
+});
+const bo = document.querySelector(".hut5 button");
+
+bo.addEventListener("click", function() {
+    alert("Liked!");
+});
+const boo = document.querySelector(".sub button");
+
+boo.addEventListener("click", function() {
+    alert("Submit Successfully!");
+});
